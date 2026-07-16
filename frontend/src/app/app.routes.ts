aae2d42b055a930340f6f670647733e29e3a9a3a
Juan Pablo <juanpablofc8.0@gmail.com>
2026-07-16 00:41:45 -0300

    chore: configura o roteamento e os importes em src/app/app.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { TaskList } from './components/task-list/task-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: TaskList
+    },
+    {
+        path: "pendentes",
+        component: TaskList
+    },
+    {
+        path: "em-andamento",
+        component: TaskList
+    },
+    {
+        path: "concluidas",
+        component: TaskList
+    },
+    {
+        path: "**",
+        redirectTo: ""
+    }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BaseUI } from './components/base-ui/base-ui';
+import { Navbar } from "./components/navbar/navbar";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [BaseUI, Navbar, RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('frontend');
+    protected readonly title = signal('frontend');
 }
